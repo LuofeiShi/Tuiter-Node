2fec2a79ebdc199ebbaeda8f6d9b96a75cfd883b
Luofei Shi <shiluofei@gmail.com>
2022-02-08 03:07:33 -0500

    enable to connect with mongo db server

--- server.ts
+++ server.ts
@@ -1,11 +1,32 @@
 import express, {Request, Response} from 'express';
-const app = express();
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+import TuitDao from "./daos/TuitDao";
+import UserDao from "./daos/UserDao";
+import mongoose from "mongoose";
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
+// // connect to the database
+// const DB_USERNAME = process.env.DB_USERNAME;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+const connectionString = 'mongodb+srv://giuseppi:supersecretpassword@cluster0.retsy.mongodb.net/Tuiter?retryWrites=true&w=majority';
+mongoose.connect(connectionString);
+
+// create RESTful web service API
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome to Luofei\'s project!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
+
+// init the tuit controller and user controller by calling constructor
+const tuiDao = new TuitDao();
+const tuitController = new TuitController(app, tuiDao);
+
+const userDao = new UserDao();
+const userController = new UserController(app, userDao);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
