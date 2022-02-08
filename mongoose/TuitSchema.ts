ca0a17147d77a8487af117fdb12548cf82504583
Luofei Shi <shiluofei@gmail.com>
2022-02-08 04:23:43 -0500

    update tuit schema

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
 const TuitSchema = new mongoose.Schema({
     tuit: {type: String, required: true},
     postOn: {type:Date, default:Date.now},
-    postBy: {type: String, required: true}
+    postBy: {type: Schema.Types.ObjectId, ref:"UserModel"}
 }, {collection:"tuits"});
 export default TuitSchema;
