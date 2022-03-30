d7d71f44a38eeebcc3bd79ee0171412eb6beb1ca
Luofei Shi <shiluofei@gmail.com>
2022-03-30 02:19:06 -0400

    update dotenv

--- server.ts
+++ server.ts
@@ -13,9 +13,9 @@
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
-require('dotenv').config({
-    path:"./.env"
-});
+// require('dotenv').config({
+//     path:"./.env"
+// });
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
