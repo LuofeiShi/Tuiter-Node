d772666c9c018c5099bcb7c14571bd21f5eacc80
Luofei Shi <shiluofei@gmail.com>
2022-03-30 18:52:22 -0400

    remove some functions that are not necessary to the implementation

--- interfaces/DislikeControllerI.ts
+++ interfaces/DislikeControllerI.ts
@@ -2,8 +2,8 @@ import {Request, Response} from "express";
 
 export default interface DislikeControllerI {
     findAllTuitsDislikedByUser (req: Request, res: Response): void;
-    userDislikesTuit (req: Request, res: Response): void;
-    userUndislikeTuit (req: Request, res: Response): void;
-    countHowManyDislikedTuit (req: Request, res: Response): void;
+    // userDislikesTuit (req: Request, res: Response): void;
+    // userUndislikeTuit (req: Request, res: Response): void;
+    // countHowManyDislikedTuit (req: Request, res: Response): void;
     userTogglesTuitDislikes (req: Request, res: Response): void;
 };
